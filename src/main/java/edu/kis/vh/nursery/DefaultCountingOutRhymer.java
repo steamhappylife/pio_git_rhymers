99545ab08fc8d90fe177018f32bad8bca2ec5784
Rafał Synowiec <255705@edu.p.lodz.pl>
2026-04-21 18:12:35 +0200

    10. Stworzenie gettera

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -11,6 +11,10 @@ public class DefaultCountingOutRhymer {
 
     private int total = EMPTY_RHYMER_INDICATOR;
 
+    public int getTotal() {
+        return total;
+    }
+
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
