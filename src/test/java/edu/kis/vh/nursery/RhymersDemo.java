e635d515c95cfbb91c83e85f27eaef5936160003
Rafał Synowiec <255705@edu.p.lodz.pl>
2026-04-21 21:27:24 +0200

    15. Projekt jest poprawny

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -3,6 +3,7 @@ package edu.kis.vh.nursery;
 import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
 import edu.kis.vh.nursery.factory.RhymersFactory;
 
+// Projekt jest poprawny
 class RhymersDemo {
 
     public static void main(String[] args) {
